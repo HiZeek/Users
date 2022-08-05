d3ab875ab275258311363d170f7b5e9a44aec477
Isaac Idibia <idibiaisaac3@gmail.com>
2022-08-05 22:24:45 +0100

    updated

--- src/Components/Users/UsersList.js
+++ src/Components/Users/UsersList.js
@@ -3,10 +3,8 @@ import Card from "../UI/Card";
 import classes from './UsersList.module.css'
 
 const UsersList = (props) => {
-  // console.log(props.users[0].name);
   return (
     <Card className={classes.users}>
-      {/* <p>{props.users[0].name}</p> */}
       <ul>
         {props.users.map((user) => (
           <li key={user.id}>
